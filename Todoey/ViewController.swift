a90904b43ae31058068e53218b4e58eb882be734
Natanail <chris.manikjr@gmail.com>
2019-07-29 22:27:59 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Chris Manik on 7/29/19.
+//  Copyright © 2019 Natanail Manik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
